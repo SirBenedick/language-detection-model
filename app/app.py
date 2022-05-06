ca387fb0d1b82e9ad768e4b001fbb0ffbc0cc963
SirBenedick <beneweb@outlook.de>
2022-05-06 16:28:24 +0200

    Added feedback loop

--- app/app.py
+++ app/app.py
@@ -1,32 +1,56 @@
 import streamlit as st
 from language_detection.lang_detector import LanguageDetector
+import requests
+API = "https://language-detection-api-v2.herokuapp.com"
 
 if __name__ == "__main__":
-    detector = LanguageDetector("data/trained_models/simple_mlp_novectorize.h5", "data/trained_models/vectorizer")
-    
-    with st.sidebar:
-        """
-        This is a simple streamlit application that guesses the language of the text passed.
-        """
+    detector = LanguageDetector(
+        "data/trained_models/simple_mlp_novectorize.h5", "data/trained_models/vectorizer")
 
-    text_input = st.text_area(
+    title = st.title('Language detection model v2')
+
+    # Input section
+    input_section_text = st.text_area(
         "Please enter sample text to detect language: ",
         value="",
-        placeholder=None,
+        placeholder="SE4AI is my favorite course",
         disabled=False,
     )
 
-    def get_text_input_value():
-        return text_input
-
-    def detect_language():
-        user_input = str(text_input)
+    detect_button = st.button("Detect language")
+    if detect_button:
+        user_input = str(input_section_text)
         language = detector.detect_language(user_input)
-        st.write("Input text: %s" % user_input)
-        st.write("Detected language is %s" % language)
+        detection_result_input = st.markdown(f'Input text: **{user_input}**')
+        detection_result_language = st.markdown(
+            f'The detected language is: **{language}**')
 
+    # Results section
+    detection_result_input = st.empty()
+    detection_result_language = st.empty()
 
-    button = st.button(
-        "Detect language",
-        on_click=detect_language
-    )
+    # Feedback section
+    feedback_headline = st.markdown('## Please provide feedback')
+    feedback_language = st.radio(
+        f'What language was "{str(input_section_text)}"?', ('English', 'German', 'Spanish'))
+
+    feedback_button = st.button("Send Feedback")
+
+    if feedback_button:
+        text = str(input_section_text)
+        label = str(feedback_language)
+        if bool(text) and bool(label):
+            if label == "English":
+                label = "en"
+            elif label == "German":
+                label = "de"
+            elif label == "Spanish":
+                label = "es"
+
+            r = requests.get(f"{API}/add?text={text}&label={label}")
+            if r.status_code == 200:
+                st.write("Successfully entered fedback. Thank you!")
+            else:
+                st.write("Something failed")
+        else:
+            st.write("Please enter a text first and detect the language!")
