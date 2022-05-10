994b69234cc2e85286cb9060d452a69f5ffa79d8
Daniel Helmrich <dahelmrich@gmail.com>
2022-05-10 12:10:19 +0200

    Check for main branch in train_test_deploy.sh

--- train_test_deploy.sh
+++ train_test_deploy.sh
@@ -2,11 +2,17 @@
 
 set -eux
 
+if [[ $(git rev-parse --abbrev-ref HEAD) != "main" ]]; then
+ echo "Must be on branch main. Exit"
+ exit 2
+fi
 if [[ -n $(git diff --name-only --cached) ]]; then
   echo "Uncommited changes in git repo, please commit or stash. Exit"
   exit 1
 fi
 
+
+git pull
 cd "$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
 
 curl -o data/feedback.csv https://language-detection-api-v2.herokuapp.com/download
