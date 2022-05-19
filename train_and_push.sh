5f497c4318eea1567e90e518bd45b794046f9e18
Daniel Helmrich <dahelmrich@gmail.com>
2022-05-19 11:01:38 +0200

    Use docker in train_and_push.sh

--- train_and_push.sh
+++ train_and_push.sh
@@ -6,7 +6,7 @@ if [[ $(git rev-parse --abbrev-ref HEAD) != "main" ]]; then
  echo "Must be on branch main. Exit"
  exit 2
 fi
-if [[ -n $(git diff --name-only --cached) ]]; then
+if [[ -n $(git diff --name-only) ]]; then
   echo "Uncommited changes in git repo, please commit or stash. Exit"
   exit 1
 fi
@@ -16,7 +16,7 @@ git pull
 cd "$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
 
 curl -o training/data/feedback.csv https://language-detection-api-v2.herokuapp.com/download
-python training/train.py --input training/data --output app/data/trained_models/
+docker run -v $(pwd)/training/data:/training/data -v $(pwd)/app/data/trained_models:/training/trained_models -it training ./train.py
 git add app/data/trained_models
 
 REPORT_DIFF=$(git diff app/data/trained_models/report.json)
