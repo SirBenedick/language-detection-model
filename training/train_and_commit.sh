02857e6529fed13338aa68268f9eea7d17f5fb49
Daniel Helmrich <dahelmrich@gmail.com>
2022-05-19 11:53:51 +0200

    Add files training/data after training to git for reproducibility

--- training/train_and_commit.sh
+++ training/train_and_commit.sh
@@ -17,6 +17,7 @@ fi
 curl -o training/data/feedback.csv https://language-detection-api-v2.herokuapp.com/download
 python training/train.py --input training/data --output app/data/trained_models
 git add app/data/trained_models
+git add training/data
 
 REPORT_DIFF=$(git diff app/data/trained_models/report.json)
 git commit -m "Update model $(date -u +'%Y-%m-%dT%H:%M')" -m "$REPORT_DIFF"
